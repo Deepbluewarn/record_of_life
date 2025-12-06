ac5dea43e20c102ce8653457ac20faae0e9940c6
bluewarn <n5lp97@gmail.com>
2025-12-06 17:34:54 +0900

    웹 버전 폭 제한 설정

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:record_of_life/features/roll/presentation/pages/home_page.dart';
 import 'package:record_of_life/shared/theme/app_theme.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() {
   runApp(ProviderScope(child: MainApp()));
@@ -12,6 +13,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: AppTheme.lightTheme, home: HomePage());
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      builder: (context, child) {
+        return Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: kIsWeb ? 480 : double.infinity,
+            ),
+            child: child,
+          ),
+        );
+      },
+      home: HomePage(),
+    );
   }
 }
